78d2b3c34362b4a51ec9d17b27dbdf326490d52e
Serafima <svp1982@mail.ru>
2019-08-15 17:59:47 +0200

    HT5

--- HT5/decomposition/src/Components/Broadcast.jsx
+++ HT5/decomposition/src/Components/Broadcast.jsx
@@ -2,23 +2,26 @@ import React from 'react';
 
 
 export default function Broadcast()  {
-    const BodyRow = props => (
-        <div className="first-row"><i>{props.icon}</i><a href={props.link}><p>{props.time}</p>{props.title}</a></div>
-    );
-
-    const BodyColumn = props => (
-        <div className="rows"> Отвечает за блок с список в эфире
-            <h2 className="card-body">{props.title}</h2>
-            <div >
-                <BodyRow time="12.00" title="Really interesting1" link="#"/>
-                <BodyRow time="15.30" title="Really interesting2" link="#"/>
-                <BodyRow time="18.30" title="Really interesting3" link="#"/>
-            </div>
+    const List = props => (
+        <div><h2 className="card-body">{props.title}</h2>
+            <ul className={props.className} style={props.style}>{props.children}</ul>
         </div>
     );
+    const ListItem = props => (
+        <li className={props.className}><i>{props.icon}</i><a href={props.link}><p>{props.time}</p>{props.title}
+        </a>{props.children}</li>
+    );
+
+
     return (
         <div className="card">
-            <BodyColumn title="TV program"/>
+           Отвечает за блок с список в эфире
+                <List className="rows" title="Эфир">
+                    <ListItem time="12.00" title="Really interesting1" link="#"/>
+                    <ListItem time="15.30" title="Really interesting2" link="#"/>
+                    <ListItem time="18.30" title="Really interesting3" link="#"/>
+                </List>
+
         </div>
     )
 }
