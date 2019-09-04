1576c65f2db947b889f4ddbd5a7aa02350b01cd8
Serafima <svp1982@mail.ru>
2019-09-04 18:46:17 +0200

    Some changes

--- HT6/crud/src/App.js
+++ HT6/crud/src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-
 import './App.css';
 
-//import Crud from "./Component/Crud";
 import Cruds from "./Component/Cruds";
 
-function App()  {
-
-        return (
-            <div className="App">
-
-<Cruds/>
-            </div>
-        );
-    }
-
+function App() {
+    return (
+        <div className="App">
+            <Cruds/>
+        </div>
+    );
+}
 
 export default App;
--- HT6/crud/src/Component/Cruds.jsx
+++ HT6/crud/src/Component/Cruds.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 
-
 export default class Cruds extends Component {
     constructor(props) {
         super(props);
