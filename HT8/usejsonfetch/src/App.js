47ad97e335ed72df31352bf80885dea1a63d903a
Serafima <svp1982@mail.ru>
2019-09-09 22:32:43 +0200

    HT8

--- HT8/usejsonfetch/src/App.js
+++ HT8/usejsonfetch/src/App.js
@@ -4,14 +4,11 @@ import './App.css';
 import Hook from "./Components/Hook";
 
 
-
 export default function App() {
-  return (<div>
-    <Hook url={process.env.REACT_APP_DATA_URL}/>
-    <hr/>
-    <Hook url={process.env.REACT_APP_ERROR_URL}/>
-    <hr/>
-    <Hook url={process.env.REACT_APP_LOADING_URL}/>
-    <hr/>
-  </div>)
+    return (<div>
+        <Hook url={process.env.REACT_APP_DATA_URL}/>
+        <Hook url={process.env.REACT_APP_ERROR_URL}/>
+        <Hook url={process.env.REACT_APP_LOADING_URL}/>
+
+    </div>)
 }
