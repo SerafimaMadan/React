c2f12787383617e3dd7787251b4a3fae3f525ad9
Serafima <svp1982@mail.ru>
2019-10-30 18:42:58 +0100

    HT9 some changes

--- HT9/crud/src/Components/PostProvider.jsx
+++ HT9/crud/src/Components/PostProvider.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, } from 'react'
 import PostsContext from '../context/PostsContext';
+import axios from "axios";
 
 export default function PostProvider(props) {
     const [posts, setPosts] = useState(['']);
-
+    useEffect(() => {
+        axios.get('http://localhost:7777/posts')
+            .then(response => setPosts(response.data));
+    },);
     return (
         <PostsContext.Provider value={posts}>
             {props.children}
